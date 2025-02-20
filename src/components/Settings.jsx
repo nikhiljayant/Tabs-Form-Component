5af00d776149e007dad472118b3546034434ea7e
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2025-02-20 23:39:40 +0530

    feat: add theme selection options to Settings component

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Settings = ({ data, setData }) => {
   return (
-    <div>Settings</div>
-  )
-}
+    <div>
+      <fieldset className="border">
+        <legend className="py-[3px] ml-[10px]">
+          Choose your favorite theme ?
+        </legend>
 
-export default Settings
+        <div className="flex flex-col gap-5">
+          <div className="flex gap-5 items-center">
+            <input type="radio" className="!h-[20px]" />
+            <label for="kraken">Kraken</label>
+          </div>
+        </div>
+      </fieldset>
+    </div>
+  );
+};
+
+export default Settings;
